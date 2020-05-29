357223d202233b4817086b6352e17933a06804ed
ferrazjoao <ferraz_joao@outlook.com>
2020-05-28 21:21:39 -0300

    Status

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text , Alert} from 'react-native';
+import Status from './src/components/Status';
+
+
+export default function App() {
+
+  return (
+      <Status text = 'Ola mundo'/>
+     
+  )
+}
---
+++ src/components/Status.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default function Status(props) {
+    return (
+        <View>
+            <Text style={{
+                textAlign: 'center',
+                fontSize: 18,
+                height: 80,
+                width: 400,
+                borderColor: '#B88E44',
+                borderWidth: 1,
+                borderRadius: 30,
+                alignSelf: 'center',
+                backgroundColor: 'black',
+                alignItems: 'center',
+                justifyContent: 'center',
+                alignContent: 'center',
+                color: 'white'
+            }}>
+                {props.text}
+            </Text>
+        </View>
+    );
+}
