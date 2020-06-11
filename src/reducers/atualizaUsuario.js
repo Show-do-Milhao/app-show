9d85ee2dd20c950f9d6a01af5fe64f4b2262d3d3
ferrazjoao <ferraz_joao@outlook.com>
2020-06-10 21:49:30 -0300

    Tela de ranking em processo

--- src/reducers/atualizaUsuario.js
+++ src/reducers/atualizaUsuario.js
@@ -3,8 +3,11 @@ const atualizaUsuario = (store = { user: {} }, action) => {
     switch (action.type) {
         case "UPDATE_USER":
             return { ...store, user: action.user };
+        case "UPDATE_RANKING":
+            return { ...store, ranking: action.ranking};
         default:
             return store;
+
     }
 };
 export default atualizaUsuario;
