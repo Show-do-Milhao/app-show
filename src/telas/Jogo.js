063397ead4e10a58086997920d74d10e7782462d
indira singh <lucindhy@gmail.com>
2020-06-24 16:04:28 -0300

    codigo refatorado

--- src/telas/Jogo.js
+++ src/telas/Jogo.js
@@ -1,205 +1,203 @@
-import React from 'react';
-import { SafeAreaView, ImageBackground, View, } from 'react-native';
-import 'react-native-gesture-handler';
-import img from '../img/background_milhao.jpg';
-import Botao from '../components/Botao';
-import Pergunta from '../components/Pergunta';
-import Alternativas from '../components/Alternativas';
-import Status from '../components/Status';
-import axios from 'axios';
-
-{ var nomeDoPlayer }
-
-
+import React from "react";
+import { SafeAreaView, ImageBackground, View } from "react-native";
+import "react-native-gesture-handler";
+import img from "../img/background_milhao.jpg";
+import Botao from "../components/Botao";
+import Pergunta from "../components/Pergunta";
+import Alternativas from "../components/Alternativas";
+import Status from "../components/Status";
+import axios from "axios";
 class Jogo extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            nickname: '',
-            score: 0,
-            acertou: 10000,
-            vidas: 3,
-            pulos: 3,
-            id: 1,
-            pergunta: '',
-            resposta_1: '',
-            resposta_2: '',
-            resposta_3: '',
-            resposta_4: '',
-            resposta_correta: '',
-            backgroundColor1: 'white',
-            backgroundColor2: 'white',
-            backgroundColor3: 'white',
-            backgroundColor4: 'white',
-            disabled: 'false'
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      score: 0,
+      acertou: 10000,
+      vidas: 3,
+      pulos: 3,
+      id: 1,
+      pergunta: {
+        pergunta: "",
+        resposta_1: "",
+        resposta_2: "",
+        resposta_3: "",
+        resposta_4: "",
+        resposta_correta: "",
+      },
+      backgroundColor1: "white",
+      backgroundColor2: "white",
+      backgroundColor3: "white",
+      backgroundColor4: "white",
+      disabled: "false",
+    };
+  }
+  pegaPergunta = (id) => {
+    axios
+      .post("https://show-do-milhao-app.herokuapp.com/questions", { id })
+      .then((res) => {
+        const [pergunta] = res.data.map((question) => ({
+          pergunta: question.question,
+          resposta_1: question.answer_1,
+          resposta_2: question.answer_2,
+          resposta_3: question.answer_3,
+          resposta_4: question.answer_4,
+          resposta_correta: question.answer_correct,
+        }));
+        console.log("Pegou com sucesso");
+        this.setState({ pergunta });
+      })
+      .catch(function (error) {
+        console.log("Miou", error);
+      });
+  };
+  resetarBackground(backgroundName) {
+    setTimeout(
+      () =>
+        this.setState({
+          [backgroundName]: "white",
+        }),
+      200
+    );
+  }
+  alternativaFoiClicada = (alternativa) => {
+    const backgroundName = "backgroundColor" + alternativa;
+    const novoId = this.state.id + 1;
+    if (this.state.pergunta.resposta_correta == alternativa) {
+      this.setState({
+        [backgroundName]: "green",
+        id: novoId,
+        score: this.state.score + this.state.acertou,
+      });
+      this.resetarBackground(backgroundName);
+      this.pegaPergunta(novoId);
+    } else {
+      this.errou();
+      if (this.state.vidas <= 0) {
+        this.salvaDados();
+        this.props.navigation.navigate("GameOver", {
+          score: this.state.score,
+        });
+      } else {
+        this.setState({
+          [backgroundName]: "red",
+          id: novoId,
+        });
+        this.resetarBackground(backgroundName);
+        this.pegaPergunta(novoId);
+      }
     }
-
-    PegaPergunta = () => {
-        axios.post('https://show-do-milhao-app.herokuapp.com/questions', {
-            id: this.state.id,
-        })
-            .then(res => {
-
-                const perguntas = res.data.map(question => question.question)
-                const resposta_1 = res.data.map(answer_1 => answer_1.answer_1)
-                const resposta_2 = res.data.map(answer_2 => answer_2.answer_2)
-                const resposta_3 = res.data.map(answer_3 => answer_3.answer_3)
-                const resposta_4 = res.data.map(answer_4 => answer_4.answer_4)
-                const resposta_correta = res.data.map(answer_correct => answer_correct.answer_correct)
-
-                this.setState({perguntas , resposta_1, resposta_2,  resposta_3, resposta_4,  resposta_correta })
-            })
-            .catch(function (error) {
-            })
-    }
-
-    Acertou() {
-        this.setState({ score: this.state.score + this.state.acertou })
-    }
-    Errou() {
-        this.setState({ vidas: this.state.vidas - 1 })
-    }
-    SalvaDados() {
-        axios.put('https://show-do-milhao-app.herokuapp.com/players', {
-            score: this.state.score,
-            nickname: this.state.nickname[0],
-        })
-
-    }
-    componentDidMount = () => {
-       this.PegaPergunta()
-       this.setState({ nickname: this.state.nickname = this.props.route.params.nickname })
-    }
-    render() {
-        return (
-            <SafeAreaView style={{ flex: 1 }} >
-                <ImageBackground source={img} style={{ flex: 1 }}>
-                    <View style={{ flex: 2, alignItems: 'center', justifyContent: 'space-around' }}>
-                        <Pergunta text={this.state.perguntas} />
-                    </View>
-                    <View style={{ flex: 3, alignItems: 'center', }}>
-                        <Alternativas textnumber='1' backgroundColor={this.state.backgroundColor1} text={this.state.resposta_1} onPress={() => {
-                            
-                            if (this.state.resposta_correta == 1) {
-                                this.setState({ backgroundColor1: this.state.backgroundColor1 = 'green', id: this.state.id + 1, score: this.state.score + this.state.acertou })
-                                this.cor = setInterval(() => this.setState({ backgroundColor1: this.state.backgroundColor1 = 'white' }), 1000);
-                                this.PegaPergunta()
-                                this.Acertou()
-                            } else {
-                                this.Errou()
-                                if (this.state.vidas <= 0) {
-                                    this.SalvaDados()
-                                    this.props.navigation.navigate('GameOver', {
-                                        score: this.state.score
-                                    })
-
-                                } else {
-                                    this.setState({ backgroundColor1: this.state.backgroundColor1 = 'red' })
-                                    this.cor = setInterval(() => this.setState({ backgroundColor1: this.state.backgroundColor1 = 'white' }), 1000);
-                                    this.setState({ id: this.state.id + 1 })
-                                    this.PegaPergunta()
-                                }
-                            }
-                        }} />
-                        <Alternativas textnumber='2' backgroundColor={this.state.backgroundColor2} text={this.state.resposta_2} onPress={() => {
-                            if (this.state.resposta_correta == 2) {
-                                this.setState({ backgroundColor2: this.state.backgroundColor2 = 'green' })
-                                this.cor = setInterval(() => this.setState({ backgroundColor2: this.state.backgroundColor2 = 'white' }), 1000);
-                                this.setState({ id: this.state.id + 1 }),
-                                this.PegaPergunta()
-                                this.Acertou()
-                            } else {
-                                this.Errou()
-                                if (this.state.vidas <= 0) {
-                                    this.SalvaDados()
-                                    this.props.navigation.navigate('GameOver', {
-                                        score: this.state.score
-                                    })
-                                } else {
-                                    this.setState({ backgroundColor2: this.state.backgroundColor2 = 'red' })
-                                    this.cor = setInterval(() => this.setState({ backgroundColor2: this.state.backgroundColor2 = 'white' }), 1000);
-                                    this.setState({ id: this.state.id + 1 })
-                                    this.PegaPergunta()
-                                }
-                            }
-                        }} />
-                        <Alternativas textnumber='3' backgroundColor={this.state.backgroundColor3} text={this.state.resposta_3} onPress={() => {
-                            if (this.state.resposta_correta == 3) {
-                                this.setState({ backgroundColor3: this.state.backgroundColor3 = 'green' })
-                                this.cor = setInterval(() => this.setState({ backgroundColor3: this.state.backgroundColor3 = 'white' }), 1000);
-                                this.setState({ id: this.state.id + 1 }),
-                                this.PegaPergunta()
-                                this.Acertou()
-                            } else {
-                                this.Errou()
-                                if (this.state.vidas <= 0) {
-                                    this.SalvaDados()
-                                    this.props.navigation.navigate('GameOver', {
-                                        score: this.state.score
-                                    })
-                                } else {
-                                    this.setState({ backgroundColor3: this.state.backgroundColor3 = 'red' })
-                                    this.cor = setInterval(() => this.setState({ backgroundColor3: this.state.backgroundColor3 = 'white' }), 1000);
-                                    this.setState({ id: this.state.id + 1 })
-                                    this.PegaPergunta()
-                                }
-                            }
-                        }} />
-                        <Alternativas textnumber='4' backgroundColor={this.state.backgroundColor4} text={this.state.resposta_4} onPress={() => {
-                            if (this.state.resposta_correta == 4) {
-                                this.setState({ backgroundColor4: this.state.backgroundColor4 = 'green' })
-                                this.cor = setInterval(() => this.setState({ backgroundColor4: this.state.backgroundColor4 = 'white' }), 1000);
-                                this.setState({ id: this.state.id + 1 }),
-                                    this.PegaPergunta()
-                                this.Acertou()
-                            } else {
-                                this.Errou()
-                                if (this.state.vidas <= 0) {
-                                    this.SalvaDados()
-                                    this.props.navigation.navigate('GameOver', {
-                                        score: this.state.score
-                                    })
-                                } else {
-                                    this.setState({ backgroundColor4: this.state.backgroundColor4 = 'red' })
-                                    this.cor = setInterval(() => this.setState({ backgroundColor4: this.state.backgroundColor4 = 'white' }), 1000);
-                                    this.setState({ id: this.state.id + 1 })
-                                    this.PegaPergunta()
-                                }
-                            }
-                        }} />
-                    </View>
-                    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', marginLeft: 20, marginRight: 20 }}>
-                        <Status text={`Vidas \n` + this.state.vidas} borderColor='red' />
-                        <Status text={`Score \n` + this.state.score} borderColor='#B88E44' />
-                        <Status text={`Acertar \n` + this.state.acertou} borderColor='green' />
-                    </View>
-                    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', marginLeft: 20, marginRight: 20 }}>
-                        <Botao text='Parar' width={140} height={40} onPress={() =>{
-                            this.SalvaDados(),
-                            this.props.navigation.navigate('FimDoJogo', {
-                                score: this.state.score
-
-                              
-                            })}} />
-
-                        <Botao text={'Pulos ' + this.state.pulos + '/3'} width={140} disabled={this.state.disabled} height={40} style={{ marginLeft: 100 }} onPress={() => {
-                            if (this.state.pulos > 0) {
-                                this.setState({ id: this.state.id + 1 }),
-                                    this.PegaPergunta(),
-                                    this.setState({ pulos: this.state.pulos - 1 })
-                            } else {
-                                this.setState({ disabled: this.state.disabled = 'true' })
-                            }
-                        }} />
-                    </View>
-                </ImageBackground>
-            </SafeAreaView>
-        )
-    }
+  };
+  errou() {
+    this.setState({ vidas: this.state.vidas - 1 });
+  }
+  salvaDados() {
+    const data = {
+      score: this.state.score,
+      nickname: this.props.route.params.nickname[0],
+    };
+    axios.put("https://show-do-milhao-app.herokuapp.com/players", data);
+  }
+  componentDidMount = () => {
+    this.pegaPergunta(this.state.id);
+  };
+  render() {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <ImageBackground source={img} style={{ flex: 1 }}>
+          <View
+            style={{
+              flex: 2,
+              alignItems: "center",
+              justifyContent: "space-around",
+            }}
+          >
+            <Pergunta text={this.state.pergunta.pergunta} />
+          </View>
+          <View style={{ flex: 3, alignItems: "center" }}>
+            <Alternativas
+              textnumber="1"
+              backgroundColor={this.state.backgroundColor1}
+              text={this.state.pergunta.resposta_1}
+              onPress={() => this.alternativaFoiClicada(1)}
+            />
+            <Alternativas
+              textnumber="2"
+              backgroundColor={this.state.backgroundColor2}
+              text={this.state.pergunta.resposta_2}
+              onPress={() => this.alternativaFoiClicada(2)}
+            />
+            <Alternativas
+              textnumber="3"
+              backgroundColor={this.state.backgroundColor3}
+              text={this.state.pergunta.resposta_3}
+              onPress={() => this.alternativaFoiClicada(3)}
+            />
+            <Alternativas
+              textnumber="4"
+              backgroundColor={this.state.backgroundColor4}
+              text={this.state.pergunta.resposta_4}
+              onPress={() => this.alternativaFoiClicada(4)}
+            />
+          </View>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginLeft: 20,
+              marginRight: 20,
+            }}
+          >
+            <Status text={`Vidas \n` + this.state.vidas} borderColor="red" />
+            <Status
+              text={`Score \n` + this.state.score}
+              borderColor="#B88E44"
+            />
+            <Status
+              text={`Acertar \n` + this.state.acertou}
+              borderColor="green"
+            />
+          </View>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginLeft: 20,
+              marginRight: 20,
+            }}
+          >
+            <Botao
+              text="Parar"
+              width={140}
+              height={40}
+              onPress={() => {
+                this.salvaDados(),
+                  this.props.navigation.navigate("FimDoJogo", {
+                    score: this.state.score,
+                  });
+              }}
+            />
+            <Botao
+              text={"Pulos " + this.state.pulos + "/3"}
+              width={140}
+              disabled={this.state.disabled}
+              height={40}
+              style={{ marginLeft: 100 }}
+              onPress={() => {
+                if (this.state.pulos > 0) {
+                  this.setState({ id: this.state.id + 1 }),
+                    this.pegaPergunta(),
+                    this.setState({ pulos: this.state.pulos - 1 });
+                } else {
+                  this.setState({ disabled: (this.state.disabled = "true") });
+                }
+              }}
+            />
+          </View>
+        </ImageBackground>
+      </SafeAreaView>
+    );
+  }
 }
 export default Jogo;
-
-
-
-
